daf396101843315a7c0f4c1f5bf5fdd048769c36
Sergei Eensalu <sergei.eensalu@gmail.com>
2025-07-06 13:41:41 +0300

    Add comments about more complex structure and why we did more simplified version.

--- src/main/java/com/accountmanagement/application/dto/AccountCreateDto.java
+++ src/main/java/com/accountmanagement/application/dto/AccountCreateDto.java
@@ -11,6 +11,30 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.Builder;
 
+
+/**
+ * Comment by S.Eensalu:
+ * For the purposes of this demo project, the Account model uses simplified flat fields:
+ * - name: as a single string
+ * - phoneNumber: raw string
+ *
+ * In a real-world system, a more robust schema would be used to support internationalization,
+ * data normalization, and extensibility.
+ *
+ * Example of more structured request format:
+ *
+ * {
+ *   "name": {
+ *     "firstName": "Sergei",
+ *     "lastName": "Eensalu"
+ *   },
+ *   "phone": {
+ *     "countryCode": "+372",
+ *     "nationalNumber": "56575305",
+ *     "isoCountry": "EE"
+ *   }
+ * }
+ */
 @Getter
 @Setter
 @NoArgsConstructor
